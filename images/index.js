ec39436f770b5dfcb5d14f0327084ad16bec7577
Shammah Ugbebor <116138132+high-Shammah@users.noreply.github.com>
2023-11-27 15:51:44 +0100

    Add files via upload

---
+++ images/index.js
@@ -0,0 +1,108 @@
+import AfangSoup from './Afang-Soup.jpg'
+import AfricanBeefStew from './African-Beef-Stew.jpg'
+import akaraandpap from './akara-and-pap.jpg'
+import amala from './amala.jpg'
+import amala2 from './amala2.jpg'
+import BangaSoup2 from './Banga-Soup-2.jpg'
+import BangaSoup from './Banga-Soup-2.jpg'
+import beans3 from './beans-3.jpg'
+import BeefStew from './Beef-Stew.jpg'
+import CatfishPeppersoup from './Catfish-Peppersoup.jpg'
+import eba from './eba.jpg'
+import EdikaikongSoup from './Edikaikong-Soup.jpg'
+import EgusiSoup from './Egusi-Soup.jpg'
+import fishsomething from './fish something.jpg'
+import fish1 from './fish-1.jpg'
+import fish2 from './fish-2.jpg'
+import fish3 from './fish-3.jpg'
+import fish4 from './fish-4.jpg'
+import FriedRice from './Fried-Rice.jpg'
+import FriendPlantainandEggmeal from './Friend-Plantainand-Eggmeal.jpg'
+import goatpeppersoup from './goat-pepper-soup.jpg'
+import GoatmeatPeppersoup from './Goatmeat-Peppersoup.jpg'
+import indomie2 from './indomie-2.jpg'
+import indomie from './indomie.jpg'
+import Jollofriceandpepperedbeef from './Jollof-rice-and-peppered-beef.jpg'
+import Jollofricewithfriedturkey from './Jollof-rice-with-fried-turkey.jpg'
+import JollofSpaghetti from './Jollof-Spaghetti.jpg'
+import killichi from './killichi.jpg'
+import LOGO from './LOGO.png'
+import MoiMoi2 from './Moi-Moi-2.jpg'
+import moimoi from './moi-moi.jpg'
+import moimoi3 from './moi-moi-3.jpg'
+import OfadaSauce from './Ofada-Sauce.jpg'
+import okrasoup from './okra-soup.jpg'
+import okrasoup1 from './okra-soup-1.jpg'
+import plaintainandfish from './plaintain-and-fish.jpg'
+import rice1 from './rice-1.jpg'
+import riceandbeans2 from './rice-and-beans-2.jpg'
+import riceandbeans from './riceandbeans.jpg'
+import sharwama from './sharwama.jpg'
+import spagetti1 from './spagetti-1.jpg'
+import starchandbanga from './starch-and-banga.jpg'
+import stew1 from './stew-1.jpg'
+import stew2 from './stew-2.jpg'
+import Stirfryspaghetti from './Stir-fry-spaghetti.jpg'
+import suya from './suya.jpg'
+import vegetablesoup from './vegetable-soup.jpg'
+import whiterice1 from './white-rice-1.jpg'
+import yamandegg3 from './yam-and-egg-3.jpg'
+import YamporridgeAsaro from './Yam-porridge-Asaro.jpg'
+import YamPorridge from './Yam-Porridge.jpg'
+
+
+export {
+    AfangSoup,
+    AfricanBeefStew,
+    akaraandpap,
+    amala,
+amala2,
+BangaSoup2,
+BangaSoup,
+beans3,
+BeefStew,
+CatfishPeppersoup,
+eba,
+EdikaikongSoup,
+EgusiSoup,
+fishsomething,
+fish1,
+fish2,
+fish3,
+fish4,
+FriedRice,
+FriendPlantainandEggmeal,
+goatpeppersoup,
+GoatmeatPeppersoup,
+indomie2,
+indomie,
+Jollofriceandpepperedbeef,
+Jollofricewithfriedturkey,
+JollofSpaghetti,
+killichi,
+LOGO,
+MoiMoi2,
+moimoi, 
+moimoi3 ,
+OfadaSauce,
+okrasoup,
+okrasoup1,
+plaintainandfish,
+rice1,
+riceandbeans2,
+riceandbeans,
+sharwama,
+spagetti1,
+starchandbanga,
+stew1,
+stew2,
+Stirfryspaghetti,
+suya,
+vegetablesoup,
+whiterice1,
+yamandegg3,
+YamporridgeAsaro,
+YamPorridge,
+   
+}
+
